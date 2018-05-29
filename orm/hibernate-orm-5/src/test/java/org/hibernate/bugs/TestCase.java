3e997592c42882d0ab2529a3c814f8ee96fb0f5f
nikowitt <github@wittenbeck-it.de>
2018-05-29 07:18:08 +0200

    removed test case

--- orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/TestCase.java
+++ orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/TestCase.java
@@ -4,30 +4,17 @@ import org.hibernate.cfg.AvailableSettings;
 import org.junit.Test;
 
 import models.common.CommonEntity;
-import models.common.security.JafSid;
-import models.common.security.User;
 
 public class TestCase extends AbstractTestCase {
 
 	public TestCase() {
 		super(CommonEntity.class, SECURITY_ENTITIES);
-		configure(c -> c.setProperty(AvailableSettings.USE_SECOND_LEVEL_CACHE, TRUE)
-				.setProperty(AvailableSettings.SHOW_SQL, TRUE).setProperty(AvailableSettings.FORMAT_SQL, FALSE));
+		configure(c -> c.setProperty(AvailableSettings.USE_SECOND_LEVEL_CACHE, TRUE));
 	}
 
 	@Test
-	public void hhh12642() {
-		doInOpenTransaction((s, tx) -> {
-			User u = new User();
-			s.save(u.setSid(save(s, new JafSid())));
-
-			CommonEntity c = save(s, new CommonEntity().setUser(u));
-
-		});
-
-		doInOpenTransaction((s, tx) -> {
-			s.createCriteria(CommonEntity.class).list();
-		});
+	public void hhXXX() {
+		
 	}
 
 }
